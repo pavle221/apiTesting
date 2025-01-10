2b9a8e8520453db2082f61e0d25b53d8522fba37
Pavle Ristić <pristic@bluecatnetworks.com>
2025-01-10 13:55:49 +0100

    Made changes to fixture file and test file

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,10 +1,15 @@
 describe('template spec', () => {
   it('passes', () => {
+    cy.intercept({
+      method: "GET",
+      url: "https://example.cypress.io/**"
+    }).as('getExamplePage')
+
     cy.visit('https://example.cypress.io')
+    cy.wait('@getExamplePage').then((intercept) => {
+      console.log('Intercepted: ', intercept)
+      expect(intercept.state).to.eq("Complete")
+    })
   })
 })
 
-describe('test', () => {
-  it
-  
-})
